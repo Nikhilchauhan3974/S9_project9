b89ed7e2416121d2f2cef0e09ee6ed78e2463383
Nikhilchauhan3974 <nikhil3974@gmail.com>
2026-02-11 12:24:15 +0530

    Add GitHub Actions deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/S9_Project9/",
+  base: '/S9_project9/',
   plugins: [react()],
 })
