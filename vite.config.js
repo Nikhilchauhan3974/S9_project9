3f86001fbf9864489641c4aff6b6c43872bcd28f
Nikhilchauhan3974 <nikhil3974@gmail.com>
2026-02-24 23:59:02 +0530

    final fix: build + vite + vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/S9_project9/',
   plugins: [react()],
-})
+  base: "/", // ← CHANGE THIS (IMPORTANT)
+});
